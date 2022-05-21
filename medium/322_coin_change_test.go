b3792913ca2a73b034504a900d571d268744ed47
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-05-21 12:07:47 +0700

    [Medium][322] Coin change

---
+++ medium/322_coin_change_test.go
@@ -0,0 +1,24 @@
+package medium
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCoinChange(t *testing.T) {
+	cases := []struct {
+		coins  []int
+		amount int
+		want   int
+	}{
+		{[]int{1, 2, 5}, 11, 3},
+		{[]int{2}, 3, -1},
+		{[]int{1}, 0, 0},
+	}
+	for _, c := range cases {
+		got := CoinChange(c.coins, c.amount)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("CoinChange(%d, %d) == %d, want %d", c.coins, c.amount, got, c.want)
+		}
+	}
+}
