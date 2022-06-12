124b061e4623ead13b3f7fe50207feeab0a31352
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-06-12 12:45:27 +0700

    Add more test cases

--- medium/1695_maximum_erasure_value_test.go
+++ medium/1695_maximum_erasure_value_test.go
@@ -8,6 +8,7 @@ func TestMaximumUniqueSubarray(t *testing.T) {
 		want int
 	}{
 		{[]int{4, 2, 4, 5, 6}, 17},
+		{[]int{5, 2, 1, 2, 5, 2, 1, 2, 5}, 8},
 	}
 	for _, c := range cases {
 		got := MaximumUniqueSubarray(c.in)
