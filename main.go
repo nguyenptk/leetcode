50ebe5075ef961e083437f83ecfa2a1b5d379958
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-06-19 10:58:53 +0700

    Remove unused code lines in main.go

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 )
 
 func main() {
 	fmt.Println("Hello, world.")
-
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println("panic occurred:", err)
-		}
-	}()
-
-	err := errors.New("invalid APIVersion value")
-	panic(err)
 }
