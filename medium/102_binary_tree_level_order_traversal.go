fcbb2eb6d8578ada4079d091deb5ccd366dc5c7a
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-07-13 15:09:17 +0700

    [Medium][102] Binary Tree Level Order Traversal

---
+++ medium/102_binary_tree_level_order_traversal.go
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+package medium
+
+func LevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	// Initialize 2D result array and queue
+	result := [][]int{}
+	q := []TreeNode{*root}
+
+	// Iterate queue
+	for len(q) > 0 {
+		currLevel := []int{}
+		for i := len(q); i > 0; i-- {
+			node := q[0]                            // First
+			q = q[1:]                               // Poll
+			currLevel = append(currLevel, node.Val) // Add current value to current array
+			if node.Left != nil {
+				q = append(q, *node.Left) // Add back node.Left to queue
+			}
+			if node.Right != nil {
+				q = append(q, *node.Right) // Add back node.Right to queue
+			}
+		}
+		result = append(result, currLevel) // Add current array to 2D result array
+	}
+
+	return result
+}
