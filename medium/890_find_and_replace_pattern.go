1e17747b2e82ba06b21eb37de524d0110f47db23
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-07-30 09:45:22 +0700

    [Medium][890] Find and Replace Pattern

---
+++ medium/890_find_and_replace_pattern.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/find-and-replace-pattern/
+package medium
+
+var ans []string
+var codex map[byte]byte
+var cipher []byte
+
+func FindAndReplacePattern(words []string, pattern string) []string {
+	ans = []string{}
+	codex = map[byte]byte{}
+	cipher = []byte(pattern)
+	for i := 0; i < len(pattern); i++ {
+		cipher[i] = translate(cipher[i])
+	}
+	for _, word := range words {
+		compare(word)
+	}
+	return ans
+}
+
+func translate(c byte) byte {
+	if _, ok := codex[c]; !ok {
+		codex[c] = byte(97 + len(codex))
+	}
+	return codex[c]
+}
+func compare(word string) {
+	codex = map[byte]byte{}
+	for i := 0; i < len(word); i++ {
+		if translate(word[i]) != cipher[i] {
+			return
+		}
+	}
+	ans = append(ans, word)
+}
