c79aa4a6659b0d60d05bb41019b006cc958c0335
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-08-01 13:00:48 +0700

    [Medium][62] Unique Paths

---
+++ medium/62_unique_paths.go
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/unique-paths/
+package medium
+
+var cache [][]int
+
+func UniquePaths(m int, n int) int {
+	cache = make([][]int, m)
+	for i := range cache {
+		cache[i] = make([]int, n)
+	}
+	return findRoute(m, n, 0, 0)
+}
+func findRoute(m, n, row, col int) int {
+	if row >= m || col >= n {
+		return 0
+	}
+
+	if row == m-1 && col == n-1 {
+		return 1
+	}
+
+	if cache[row][col] > 0 {
+		return cache[row][col]
+	}
+
+	down := findRoute(m, n, row+1, col)
+	right := findRoute(m, n, row, col+1)
+
+	cache[row][col] = down + right
+
+	return cache[row][col]
+}
