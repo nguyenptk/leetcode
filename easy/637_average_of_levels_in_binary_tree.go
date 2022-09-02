d66e9ee2f58dc5e74f41eb5fc081014c873ebf4a
Nguyen  Phan Tran Khoi <nguyen.phan@grabtaxi.com>
2022-09-02 15:25:01 +0700

    [Easy][637] Average of Levels in Binary Tree

---
+++ easy/637_average_of_levels_in_binary_tree.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+package easy
+
+func AverageOfLevels(root *TreeNode) []float64 {
+	result := []float64{}
+	queue := []TreeNode{*root}
+	for len(queue) > 0 {
+		count := len(queue)
+		sum := float64(0)
+		for i := 0; i < count; i++ {
+			curr := queue[0]  // First
+			queue = queue[1:] // Poll
+			sum += float64(curr.Val)
+			if curr.Left != nil {
+				queue = append(queue, *curr.Left)
+			}
+			if curr.Right != nil {
+				queue = append(queue, *curr.Right)
+			}
+		}
+		result = append(result, sum/float64(count))
+	}
+	return result
+}
---
+++ easy/637_average_of_levels_in_binary_tree_test.go
@@ -0,0 +1,59 @@
+package easy
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestAverageOfLevels(t *testing.T) {
+	cases := []struct {
+		in   *TreeNode
+		want []float64
+	}{
+		// test case 1
+		{
+			&TreeNode{ // [3,9,20,null,null,15,7]
+				Val: 3,
+				Left: &TreeNode{
+					Val: 9,
+				},
+				Right: &TreeNode{
+					Val: 20,
+					Left: &TreeNode{
+						Val: 15,
+					},
+					Right: &TreeNode{
+						Val: 7,
+					},
+				},
+			},
+			[]float64{3.00000, 14.50000, 11.00000},
+		},
+		// test case 2
+		{
+			&TreeNode{ // [3,9,20,15,7]
+				Val: 3,
+				Left: &TreeNode{
+					Val: 9,
+				},
+				Right: &TreeNode{
+					Val: 20,
+					Left: &TreeNode{
+						Val: 15,
+					},
+					Right: &TreeNode{
+						Val: 7,
+					},
+				},
+			},
+			[]float64{3.00000, 14.50000, 11.00000},
+		},
+	}
+	for _, c := range cases {
+		got := AverageOfLevels(c.in)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("AverageOfLevels(%s) == %f, want %f", fmt.Sprint(c.in), got, c.want)
+		}
+	}
+}
