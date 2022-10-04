016e0416d114dcf9f5ead5c698e8d5551ee87ca9
nguyenptk <nguyenptk@gmail.com>
2022-10-04 13:02:55 +0700

    [Easy][112] Path Sum

---
+++ easy/112_path_sum.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/path-sum/
+package easy
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	return recurPathSum(root, targetSum)
+}
+
+func recurPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	result := false
+	subSum := targetSum - root.Val
+
+	if subSum == 0 && root.Left == nil && root.Right == nil {
+		return true
+	}
+	if root.Left != nil {
+		result = result || recurPathSum(root.Left, subSum)
+	}
+	if root.Right != nil {
+		result = result || recurPathSum(root.Right, subSum)
+	}
+
+	return result
+}
