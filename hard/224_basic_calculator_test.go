68727af43ef7fa41d42100382da487eef8a7bc37
nguyenptk <nguyenptk@gmail.com>
2022-11-20 21:47:15 +0700

    [Hard][224] Basic Calculator

---
+++ hard/224_basic_calculator_test.go
@@ -0,0 +1,20 @@
+package hard
+
+import "testing"
+
+func TestCalculate(t *testing.T) {
+	cases := []struct {
+		s    string
+		want int
+	}{
+		{"1 + 1", 2},
+		{" 2-1 + 2 ", 3},
+		{"(1+(4+5+2)-3)+(6+8)", 23},
+	}
+	for _, c := range cases {
+		got := Calculate(c.s)
+		if got != c.want {
+			t.Errorf("Calculate(%s) == %d, want %d", c.s, got, c.want)
+		}
+	}
+}
